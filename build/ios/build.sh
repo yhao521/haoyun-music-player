224de63198a940b441ca41500c3c8033a7deb575
yanghao <791475014@qq.com>
2026-04-08 16:47:09 +0800

    chore: bump version to 0.0.17

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="haoyun-music-player"
 BUNDLE_ID="com.haoyun.musicplayer"
-VERSION="0.0.16"
-BUILD_NUMBER="0.0.16"
+VERSION="0.0.17"
+BUILD_NUMBER="0.0.17"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
