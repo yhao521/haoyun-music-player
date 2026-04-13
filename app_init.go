f3a391e6713d73d79b2752fc3464c6c055abd0db
yanghao <791475014@qq.com>
2026-04-13 20:35:38 +0800

    chore: bump version to 0.0.35

--- app_init.go
+++ app_init.go
@@ -14,7 +14,7 @@ import (
 )
 
 // AppVersion 应用版本信息
-const AppVersion = "0.0.34"
+const AppVersion = "0.0.35"
 
 //go:embed all:frontend/dist
 var assets embed.FS
--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="haoyun-music-player"
 BUNDLE_ID="com.haoyun.musicplayer"
-VERSION="0.0.34"
-BUILD_NUMBER="0.0.34"
+VERSION="0.0.35"
+BUILD_NUMBER="0.0.35"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
